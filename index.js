9f8736b185714a04b9fa5358cd948f68306d154b
Jerin-K-Jose <jerinkjos@gmail.com>
2022-01-16 11:02:20 +0530

    home css and new page

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-let unreadContainer = document.getElementById('unread-list');
-let readContainer = document.getElementById('read-list');
+// let unreadContainer = document.getElementById('unread-list');
+// let readContainer = document.getElementById('read-list');
 
-count = 0;
+// notes = {
+//     s1:{
+//         name:"Doe",
+//         note_image_path:"https://unsplash.com/photos/FGWpR-O9QlQ"
+//     },
+//     s2:{
+//         name:"Smith",
+//         note_image_path:"https://unsplash.com/photos/FGWpR-O9QlQ"
+//     },
+//     s3:{
+//         name:"Jones",
+//         note_image_path:"https://unsplash.com/photos/FGWpR-O9QlQ"
+//     },
+// };
+// let text = JSON.stringify(notes);
 
-student_note = {
-    id: 0,
-    note_image: document.createElement("img"),
-    name: '',
-    isread: false,
-    note_read:function(){
-        this.isread = true;
-    }
-};
+// student_notes = JSON.parse(text);
+// console.log(student_notes);
 
-notes = {
-    s1:{
-        name:"Doe",
-        note_image_path:"https://unsplash.com/photos/FGWpR-O9QlQ"
-    },
-    s2:{
-        name:"Smith",
-        note_image_path:"https://unsplash.com/photos/FGWpR-O9QlQ"
-    },
-    s3:{
-        name:"Jones",
-        note_image_path:"https://unsplash.com/photos/FGWpR-O9QlQ"
-    },
-};
-let text = JSON.stringify(notes);
+// for(students in student_notes){
+//     for(key in students){
+//         console.log();
+//     }
+// }
 
-student_notes = JSON.parse(text);
-console.log(student_notes);
 
-for(students in student_notes){
-    for(key in students){
-        console.log();
-    }
+//modal popup
+
+let student_modal_3 = document.getElementById("s3");
+let student_modal_4 = document.getElementById("s4");
+let student_modal_5 = document.getElementById("s5");
+
+let new_page_stud_id;
+
+function newPage(id){
+  location.href = "student_note.html";
+  new_page_stud_id = id;
+  
 }
-
-
 
