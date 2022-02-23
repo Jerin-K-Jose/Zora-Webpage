ff8adc1f29cb6d79ed1021521605ed9a14f78945
Jerin-K-Jose <69309400+Jerin-K-Jose@users.noreply.github.com>
2022-02-23 22:32:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-// let unreadContainer = document.getElementById('unread-list');
-// let readContainer = document.getElementById('read-list');
 
-// notes = {
-//     s1:{
-//         name:"Doe",
-//         note_image_path:"https://unsplash.com/photos/FGWpR-O9QlQ"
-//     },
-//     s2:{
-//         name:"Smith",
-//         note_image_path:"https://unsplash.com/photos/FGWpR-O9QlQ"
-//     },
-//     s3:{
-//         name:"Jones",
-//         note_image_path:"https://unsplash.com/photos/FGWpR-O9QlQ"
-//     },
-// };
-// let text = JSON.stringify(notes);
-
-// student_notes = JSON.parse(text);
-// console.log(student_notes);
-
-// for(students in student_notes){
-//     for(key in students){
-//         console.log();
-//     }
-// }
-
-
-//modal popup
 
 let student_modal_3 = document.getElementById("s3");
 let student_modal_4 = document.getElementById("s4");
